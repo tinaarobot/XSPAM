798455024837fa188e9e2d6d860817030113c39e
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-25 11:52:50 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -21,7 +21,7 @@ from datetime import datetime
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» __ᴀʟᴛʀᴏɴ__")
+        altron = await e.reply(f"» __ᴀʟᴏɴᴇ__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
         await altron.edit(f"__🤖 ᴘɪɴɢ__\n» `{mp} ᴍꜱ`")
