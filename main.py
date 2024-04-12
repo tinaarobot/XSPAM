5eaa51460d7e9989a85bca22ec054b01e76c708d
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-12 22:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import urllib3
 
 
 from pathlib import Path
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+from config import X1
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
@@ -16,37 +16,27 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def load_plugins(plugin_name):
-    path = Path(f"AltBots/modules/{plugin_name}.py")
-    spec = importlib.util.spec_from_file_location(f"AltBots.modules.{plugin_name}", path)
+    path = Path(f"ROYEDITX/modules/{plugin_name}.py")
+    spec = importlib.util.spec_from_file_location(f"ROYEDITX.modules.{plugin_name}", path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltBots.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    sys.modules["ROYEDITX.modules." + plugin_name] = load
+    print("ROYEDITX has Imported " + plugin_name)
 
 
-files = glob.glob("AltBots/modules/*.py")
+files = glob.glob("ROYEDITX/modules/*.py")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @ALONE_WAS_BOT")
+print("♥︎ Bot Deployed Successfully.")
 
 
 async def main():
     await X1.run_until_disconnected()
-    await X2.run_until_disconnected()
-    await X3.run_until_disconnected()
-    await X4.run_until_disconnected()
-    await X5.run_until_disconnected()
-    await X6.run_until_disconnected()
-    await X7.run_until_disconnected()
-    await X8.run_until_disconnected()
-    await X9.run_until_disconnected()
-    await X10.run_until_disconnected()
-
-
+    
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
