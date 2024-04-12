cb9e54c0ed516bb3f96fa9637512bd09181ccb62
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-04-12 20:51:15 +0530

    Update leave.py

--- AltBots/modules/leave.py
+++ AltBots/modules/leave.py
@@ -15,8 +15,7 @@ from telethon.tl.functions.channels import LeaveChannelRequest
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 async def leave(e):
-    if e.sender_id in SUDO_USERS:
-
+    if e.sender_id == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
         if len(e.text) > 7:
             event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
             mkl = e.text.split(" ", 1)
