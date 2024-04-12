f32e96a1ab6dec8a142d0e0ec5fe8b7487ec1947
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-12 22:20:35 +0530

    Create leave.py

---
+++ ROYEDITX/modules/leave.py
@@ -0,0 +1,28 @@
+from config import X1, SUDO_USERS, CMD_HNDLR as hl
+
+from telethon import events
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+from pyrogram import enums
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+async def leave(e):
+    if e.sender_id == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
+        if len(e.text) > 7:
+            event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+            mkl = e.text.split(" ", 1)
+            try:
+                await event.client(LeaveChannelRequest(int(mkl[1])))
+            except Exception as e:
+                await event.edit(str(e))
+        else:
+             if e.is_private:
+                  alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
+                  await e.reply(alt)
+             else:
+                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+                  try:
+                      await event.client(LeaveChannelRequest(int(e.chat_id)))
+                  except Exception as e:
+                      await event.edit(str(e))
+                    
