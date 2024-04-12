03de8b34fc006f6c6ba63dda2a1798598de73177
♡︎⛧‌ٖٖٖٖٖٖٜٖ 𝐂𝐔𝐓𝐄 ꨄ︎ 𝐁𝐀𝐂𝐇𝐇𝐀⛧‌ٖٖٖٖٖٖٜٖٖٖᥫ᭡ <148250637+CUTE-BACHHA@users.noreply.github.com>
2024-04-12 20:52:11 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -2,7 +2,7 @@ import asyncio
 import heroku3
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, HEROKU_API_KEY, HEROKU_APP_NAME, CMD_HNDLR as hl
-
+from pyrogram import enums
 from datetime import datetime
 
 from telethon import events
@@ -20,7 +20,7 @@ from telethon.errors import ForbiddenError
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%slogs(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%slogs(?: |$)(.*)" % hl))
 async def logs(legend):
-    if legend.sender_id == OWNER_ID:
+    if legend.sender_id == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
         if (HEROKU_APP_NAME is None) or (HEROKU_API_KEY is None):
             await legend.reply(
                 legend.chat_id,
