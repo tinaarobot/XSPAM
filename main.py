35160aca17e306dc19b3851c131d37943ba6e6a2
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-13 11:43:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import asyncio
 import logging
 import importlib
 import urllib3
-
-
 from pathlib import Path
 from config import X1
-
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 def load_plugins(plugin_name):
     path = Path(f"ROYEDITX/modules/{plugin_name}.py")
@@ -32,8 +28,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("♥︎ Bot Deployed Successfully.")
-
+print("♥︎ Bot Deployed Successfully, By - @ROY_EDITX")
 
 async def main():
     await X1.run_until_disconnected()
