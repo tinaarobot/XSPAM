c3d3076dd44cbbad706388feaf937a614d8e53a1
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-13 11:42:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import logging
-
 from telethon import TelegramClient
-
 from os import getenv
 from ROYEDITX.data import AVISHA
 
-
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
-CMD_HNDLR = getenv("CMD_HNDLR", default=".")
+API_ID = getenv("API_ID", "18136872")
+API_HASH = getenv("API_HASH", "312d861b78efcd1b02183b2ab52a83a4")
+CMD_HNDLR = getenv("CMD_HNDLR", default="/")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
@@ -22,8 +18,5 @@ for x in AVISHA:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="6922271843"))
 SUDO_USERS.append(OWNER_ID)
-
-
-# ------------- CLIENTS -------------
 
 X1 = TelegramClient('X1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
