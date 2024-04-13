543c48c378d44945991ef51faea4f3094515f7ea
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-13 12:06:40 +0530

    Update leave.py

--- ROYEDITX/modules/leave.py
+++ ROYEDITX/modules/leave.py
@@ -9,7 +9,7 @@ from pyrogram import enums
 async def leave(e):
     if e.sender_id == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
         if len(e.text) > 7:
-            event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+            event = await e.reply("✦ ʟᴇᴀᴠɪɴɢ...")
             mkl = e.text.split(" ", 1)
             try:
                 await event.client(LeaveChannelRequest(int(mkl[1])))
@@ -17,10 +17,10 @@ async def leave(e):
                 await event.edit(str(e))
         else:
              if e.is_private:
-                  alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
+                  alt = f"**✦ ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ.**\n\n● {hl}leave ➥ <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n● {hl}leave ➥ ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
                   await e.reply(alt)
              else:
-                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+                  event = await e.reply("✦ ʟᴇᴀᴠɪɴɢ...")
                   try:
                       await event.client(LeaveChannelRequest(int(e.chat_id)))
                   except Exception as e:
